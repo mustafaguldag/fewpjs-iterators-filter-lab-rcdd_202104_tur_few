0ec3bc4eeb7fe313e60b8b8aac3aaee163dafbd2
mustafaguldag <guldag.mustafaa@gmail.com>
2021-05-12 15:06:07 +0300

    commit tree c90c124d58ca2e8ea4e29311903b10bf8dbf469a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+
+
+
+function findMatching(array, name){
+
+
+return array.filter(driverName => { return driverName.toLowerCase() === name.toLowerCase()
+}
+)
+};
+
+function fuzzyMatch(array, name){
+    return array.filter(driverName => {return driverName[0] === name[0] })
+
+};
+
+function matchName(array, name){
+    return array.filter(driverName => {
+        return driverName.name === name
+      })
+
+
+};
